e9afa4a5644ac6e192f3bfc3a4a809aad5b24c05
saikeerthip <keerthijkb@gmail.com>
2016-03-27 11:06:03 -0400

    web.xml

--- src/main/java/com/web/daoExample/MyAccountDAO.java
+++ src/main/java/com/web/daoExample/MyAccountDAO.java
@@ -4,6 +4,7 @@ import com.web.MyAccount;
 import com.web.model.Account;
 import org.springframework.security.access.method.P;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +13,22 @@ import java.util.List;
  * Created by devendra on 3/14/2016.
  */
 public class MyAccountDAO {
+
+    private DataSource dataSource;
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     public void createAccount (int id, String firstName, String lastName, int balance, String bankName){
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost5432/postgis_22_sample", "postgres", "123456");
+            Connection conn = dataSource.getConnection();
+           // Class.forName("org.postgresql.Driver");
+            //Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost5432/postgis_22_sample", "postgres", "123456");
             PreparedStatement preparedStatement = conn.prepareStatement("Insert INTO myAccount (id, firstname, lastname, balance, bankname) VALUES (?,?,?,?,?)");
             preparedStatement.setInt(1,id);
             preparedStatement.setString(2, firstName);
@@ -25,16 +38,16 @@ public class MyAccountDAO {
             int result = preparedStatement.executeUpdate();
             preparedStatement.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        }  catch (SQLException e) {
             e.printStackTrace();
         }
     }
     public void createAccount(MyAccount account) {
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost5432/postgis_22_sample", "postgres", "123456");
+
+            Connection conn = dataSource.getConnection();
+           // Class.forName("org.postgresql.Driver");
+            //Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost5432/postgis_22_sample", "postgres", "123456");
             PreparedStatement preparedStatement = conn.prepareStatement("INSERT  INTO myAccount(id, firstname, lastname, balance, bankname)VALUES (?,?,?,?,?)");
             preparedStatement.setInt(1,account.getId());
             preparedStatement.setString(2,account.getFirstName());
@@ -44,17 +57,16 @@ public class MyAccountDAO {
             int result= preparedStatement.executeUpdate();
             preparedStatement.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        }  catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
     public void updateAccount(MyAccount account){
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
+            Connection conn = dataSource.getConnection();
+           // Class.forName("org.postgresql.Driver");
+            //Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
             PreparedStatement preparedStatement = conn.prepareStatement("UPDATE myAccount SET firstname=?, lastname=?, balance=?, bankname=? WHERE id=?");
             preparedStatement.setInt(5,account.getId());
             preparedStatement.setString(1,account.getFirstName());
@@ -65,8 +77,6 @@ public class MyAccountDAO {
             System.out.println(result);
             preparedStatement.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -75,30 +85,28 @@ public class MyAccountDAO {
 
     public void deleteAccount(MyAccount account){
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
+            Connection conn = dataSource.getConnection();
+            //Class.forName("org.postgresql.Driver");
+            //Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
             PreparedStatement preparedStatement = conn.prepareStatement("DELETE from myAccount WHERE id=?");
             preparedStatement.setInt(1, account.getId());
             int result = preparedStatement.executeUpdate();
             preparedStatement.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
     public MyAccount findAccount(int id){
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
+            Connection conn = dataSource.getConnection();
+           // Class.forName("org.postgresql.Driver");
+            //Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
             PreparedStatement preparedStatement = conn.prepareStatement("SELECT from myAccount WHERE id=?");
             preparedStatement.setInt(1,id);
             int result = preparedStatement.executeUpdate();
             preparedStatement.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -108,8 +116,9 @@ public class MyAccountDAO {
     public List<MyAccount> findAll(){
         List<MyAccount> accounts = new ArrayList<>();
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
+            Connection conn = dataSource.getConnection();
+            //Class.forName("org.postgresql.Driver");
+            //Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgis_22_sample", "postgres", "123456");
             Statement statement = conn.createStatement();
             ResultSet rs = statement.executeQuery("SELECT  * FROM myAccount");
             while ((rs.next())){
@@ -123,8 +132,6 @@ public class MyAccountDAO {
             }
             statement.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
